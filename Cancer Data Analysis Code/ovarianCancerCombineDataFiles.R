37c0df3afbb3dbc1028ccf841fb824d2cd8d1f43
Aviv Bergman, Ph.D <39968201+AvivLab@users.noreply.github.com>
2022-02-04 17:44:20 -0500

    Delete ovarianCancerCombineDataFiles.R

--- Cancer Data Analysis Code/ovarianCancerCombineDataFiles.R
+++
@@ -1,6 +0,0 @@
-# Created by Maryl Lambros 9/24/19 to import Ovarian sc-RNA-seq data
-# and combine all the raw data files so can preprocess and analyze
-# like Head & Neck cancer dataset.
-setwd("../Desktop/BergmanLabRotation/SingleCellSequencingData/IdentificationOfGradeAndOriginSpecificCellPopulationsInSerousEpithelialOvarianCancerBySingleCellRNAseq/GSE118828_RAW/")
-
-dataset <- read.delim("GSM3348320_V00331151_Metastatic_S1.counts.umiCounts.aboveBackground.table.csv",header = FALSE, stringsAsFactors=F, sep=",")
